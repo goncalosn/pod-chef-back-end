51e2d746058d621aa328db8eca4da9ff473fd1b1
NoPalm0il <jpedrocr2000@hotmail.com>
2021-06-06 15:57:07 +0100

    clean up

--- pkg/auth.go
+++ pkg/auth.go
@@ -54,6 +54,7 @@ func IsAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
+//GenerateTokenIV generates random bytes into a buffer, this creates the IV for the user
 func GenerateTokenIV() []byte {
 	token := make([]byte, aes.BlockSize)
 	if _, err := rand.Read(token); err != nil {
@@ -62,12 +63,14 @@ func GenerateTokenIV() []byte {
 	return token
 }
 
+//EncryptPassword encrypts plaintext password into AES-CBC, return a byte array
 func EncryptPassword(rawPassword string, iv []byte, key []byte) []byte {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		fmt.Println("key error1", err)
 	}
 
+	// cbc block
 	ecb := cipher.NewCBCEncrypter(block, iv)
 	content := []byte(rawPassword)
 	content = PKCS5Padding(content)
@@ -77,6 +80,7 @@ func EncryptPassword(rawPassword string, iv []byte, key []byte) []byte {
 	return crypted
 }
 
+//PKCS5Padding pads a byte array to 16 multiples (%16 == 0)
 func PKCS5Padding(ciphertext []byte) []byte {
 	padding := aes.BlockSize - len(ciphertext)%aes.BlockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
